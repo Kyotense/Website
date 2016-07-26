daff3abb2735a184003848494963301572db09d5
Chase Carthen <chasec2331@gmail.com>
2016-07-26 11:40:30 -0700

    Initial functionality added complete

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<username>\w{0,50})$', views.IndexView.as_view(), name='index'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -12,6 +12,28 @@ class IndexView(generic.ListView):
     template_name = 'shop/index.html'
     context_object_name = 'products'
     model = Product
+    order = ["company","description","pub_date","name","price","company__company_name"]
+
+    def pages(self):
+    	return int(len(Product.objects.all())/6) + 1
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Product.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        """Return the last sixteen published questions."""
+
+        order_by = "pub_date"
+
+        start = 0
+        end = 6
+
+        # ?browse = whatever
+        if self.request.GET.get("order_by"):
+        	order_by = (self.request.GET.get("order_by"))
+
+        	if not order_by in self.order:
+        		order_by = "pub_date"
+       	if self.request.GET.get("page"):
+       		page = int(self.request.GET.get("page"))
+       		start = (page-1)*6
+       		end = 6 * page
+      	# urls view can be used for args and kwargs...
+        #print (self.username)
+        return Product.objects.order_by(order_by)[start:end] #filter(pub_date__lte=timezone.now())
